b235e9e1ec59398b093c356535f04145465997d7
pskocir <pato.skocir@gmail.com>
2023-01-08 18:26:22 -0300

    Validacion de formularios y botones

--- js/formulario.js
+++ js/formulario.js
@@ -1,25 +1,32 @@
-document
-  .getElementById("btn-enviar")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    const userName = document.getElementById("userName").value;
-    const textEmail = document.getElementById("textEmail").value;
-    const userComment = document.getElementById("userComment").value;
+document.getElementById("btn-enviar").addEventListener("click", function (event) {
+  event.preventDefault();
+  const userName = document.getElementById("userName").value;
+  const textEmail = document.getElementById("textEmail").value;
+  const userComment = document.getElementById("userComment").value;
 
-     // Valida los campos del formulario
-     if (userName.length < 5 || userName === "" || textEmail.length < 1 || userComment.length < 1) {
-        Swal.fire({
-          icon: 'error',
-          text: 'Hay errores en el formulario'
-        });
-        return;
-      }
-      
-      
-      // Muestra los valores en una alerta
+  if (userName.length < 5 || userName === "" || textEmail.length < 1 || userComment.length < 1) {
+    Swal.fire({
+      icon: 'error',
+      text: 'Hay errores en el formulario'
+    }).then(function() {
+      // Limpia el formulario 
+      document.getElementById("userName").value = "";
+      document.getElementById("textEmail").value = "";
+      document.getElementById("userComment").value = "";
+    });
+    return;
+  }
+  
+  // Muestra los valores en una alerta
   Swal.fire({
     title: "Datos del formulario enviado",
-    text: "Nombre y Apellido: " + userName + ", Consulta: " + userComment +  ", Mail: " + textEmail,
+    text: "Gracias " + userName + " por realizar la consulta, en breve nos comunicaremos a " + textEmail + " para responder su consulta",
     icon: "success"
+  }).then(function() {
+    // Limpia el formulario 
+    document.getElementById("userName").value = "";
+    document.getElementById("textEmail").value = "";
+    document.getElementById("userComment").value = "";
   });
-  });
+  
+});
--- js/main.js
+++ js/main.js
@@ -80,7 +80,6 @@ const mantaBsas = new Producto(
 
 ////////////////////////////////////////////////////////
 
-
 //////////Creamos array de producto y lo pusheamos.////////
 
 arrayProducto = [];
@@ -133,10 +132,9 @@ const mostrarProductos = () => {
 
     containerProductos.appendChild(card);
 
- ////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////
 
-
- //////////Agregar productos al carrito:
+    //////////Agregar productos al carrito:
 
     const boton = document.getElementById(`boton${producto.id}`);
     boton.addEventListener("click", () => {
@@ -146,19 +144,18 @@ const mostrarProductos = () => {
         duration: 3000,
         gravity: "top",
         position: "right",
-        style:
-        {
-            background: "#af9274",
-        }
-    }).showToast();
+        style: {
+          background: "#af9274",
+        },
+      }).showToast();
     });
 
     const botonMenos = document.getElementById(`botonMenos${producto.id}`);
 
     botonMenos.addEventListener("click", () => {
       disminuirCantidad(producto.id);
+      
     });
-
   });
 };
 
@@ -166,12 +163,15 @@ const mostrarProductos = () => {
 
 mostrarProductos();
 
+
+
 //Función agregar al carrito:
 
 const agregarAlCarrito = (id) => {
   const productoEnCarrito = carrito.find((producto) => producto.id === id);
   if (productoEnCarrito) {
     productoEnCarrito.cantidad++;
+    localStorage.setItem("carrito", JSON.stringify(carrito));
   } else {
     const producto = arrayProducto.find((producto) => producto.id === id);
     if (producto.eliminado) {
@@ -185,61 +185,10 @@ const agregarAlCarrito = (id) => {
   calcularTotal();
 };
 
-//Funcion para disminuir cantidad
-const disminuirCantidad = (id) => {
-  const productoEnCarrito = carrito.find((producto) => producto.id === id);
-  if (productoEnCarrito && productoEnCarrito.cantidad > 0) {
-    
-    productoEnCarrito.cantidad--;
-    Toastify({
-      text: "UNIDAD ELIMINADA",
-      duration: 3000,
-      gravity: "top",
-      position: "right",
-      style:
-      {
-          background: "#af9274",
-      }
-  }).showToast();
-  } else {
-    const producto = arrayProducto.find((producto) => producto.id === id);
-    carrito.push(producto);
-    //Trabajamos con el localStorage:
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-  }
-  calcularTotal();
-};
 
-
-
-//MOSTRAR EL CARRITO DE COMPRAS.
-
-const contenedorCarrito = document.getElementById("contenedorCarrito");
-
-const verCarrito = document.getElementById("verCarrito");
-
-verCarrito.addEventListener("click", () => {
-  
-  mostrarCarrito();
-});
-
-
-//Mostramos mensaje con el total de la compra:
-
-
-
-const calcularTotal = () => {
-  let totalCompra = 0;
-  carrito.forEach((producto) => {
-    totalCompra += producto.precio * producto.cantidad;
-  });
-  
-  return totalCompra;
-};
-
+//Funcion mostrar carrito de compras
 const mostrarCarrito = () => {
   contenedorCarrito.innerHTML = "";
-
   const tabla = document.createElement("table");
   tabla.classList.add("table", "table-striped");
   tabla.innerHTML = `
@@ -255,16 +204,16 @@ const mostrarCarrito = () => {
   `;
 
   carrito.forEach((producto) => {
-    if(producto.cantidad > 0){
-    const fila = document.createElement("tr");
-    fila.innerHTML = `
+    if (producto.cantidad > 0) {
+      const fila = document.createElement("tr");
+      fila.innerHTML = `
       <td>${producto.descripcion}</td>
       <td>${producto.cantidad}</td>
       <td>${producto.precio}</td>
       <td>${producto.cantidad * producto.precio}</td>
     `;
-    tabla.appendChild(fila);
-  }
+      tabla.appendChild(fila);
+    }
   });
 
   tabla.innerHTML += `
@@ -277,47 +226,103 @@ const mostrarCarrito = () => {
         <th scope="col">${calcularTotal()}</th>
       </tr>
       <tr>
-      <td colspan="4">
-        <button id="btnFinalizarCompra" class="btn btn-destacado">Finalizar compra</button>
-      </td>
+      
     </tr>
     </tfoot>
   `;
 
   contenedorCarrito.appendChild(tabla);
+
+  //Si el carrito está vacio, deshabilita el boton de finalizar compra.
+
+  if (carrito.length === 0) {
+    btnFinalizarCompra.removeAttribute("disabled");
+  }
+
   
 };
 
-//funciona para finalizar compra 
 
-const btnFinalizarCompra = document.querySelector("#btnFinalizarCompra");
-btnFinalizarCompra.addEventListener("click", () => {
-  swal.Fire({
-    title: "¿Deseas finalizar la compra?",
-    text: "Una vez finalizada, no podrás agregar ni eliminar productos del carrito",
-    icon: "warning",
-    buttons: ["Cancelar", "Aceptar"],
-  }).then((result) => {
-    if (result) {
-      // Si se hizo click en "Aceptar", mostramos un cartel de éxito
-      swal.Fire({
-        title: "Compra finalizada",
-        text: "Gracias por tu compra",
-        icon: "success",
-      });
-      // Deshabilitamos el botón "Finalizar compra" para evitar que se vuelva a hacer click
-      btnFinalizarCompra.setAttribute("disabled", true);
-      // Vaciamos el carrito
-      carrito = [];
-      // Actualizamos el almacenamiento local
-      localStorage.setItem("carrito", JSON.stringify(carrito));
-      // Ocultamos la tabla del carrito
-      tabla.style.display = "none";
-    }
-  });
+//MOSTRAR EL CARRITO DE COMPRAS.
+
+const contenedorCarrito = document.getElementById("contenedorCarrito");
+const verCarrito = document.getElementById("verCarrito");
+verCarrito.addEventListener("click", () => {
+  mostrarCarrito();
 });
 
+//Funcion para disminuir cantidad
+const disminuirCantidad = (id) => {
+  const productoEnCarrito = carrito.find((producto) => producto.id === id);
+  if (productoEnCarrito && productoEnCarrito.cantidad > 0) {
+    productoEnCarrito.cantidad--;
+    Toastify({
+      text: "UNIDAD ELIMINADA",
+      duration: 3000,
+      gravity: "top",
+      position: "right",
+      style: {
+        background: "#af9274",
+      },
+    }).showToast();
+  } else {
+    const producto = arrayProducto.find((producto) => producto.id === id);
+    carrito.push(producto);
+    //Trabajamos con el localStorage:
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  }
+  calcularTotal();
+};
 
+
+
+//Mostramos mensaje con el total de la compra:
+
+const calcularTotal = () => {
+  let totalCompra = 0;
+  carrito.forEach((producto) => {
+    totalCompra += producto.precio * producto.cantidad;
+  });
+  return totalCompra;
+};
+
+
+//funcion para finalizar compra al hacer clic.
+
+const btnFinalizarCompra = document.getElementById("finalizarCompra");
+btnFinalizarCompra.addEventListener("click", () => {
+  if (carrito.length === 0) {
+    Swal.fire({
+      title: "Error",
+      text: "No hay productos en el carrito",
+      icon: "error",
+    });
+    return;
+  } else {
+    console.log(carrito);
+    Swal.fire({
+      title: "¿Deseas finalizar la compra?",
+      text: "Una vez finalizada, no podrás agregar ni eliminar productos del carrito",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "ACEPTAR",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("COMPRA REALIZADA CON EXITO");
+        // Deshabilitamos el botón "Finalizar compra" para evitar que se vuelva a hacer click
+        btnFinalizarCompra.setAttribute("disabled", true);
+        // Vaciamos el carrito
+        carrito = [];
+        // Actualizamos el almacenamiento local
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+        // Ocultamos la tabla del carrito
+        tabla.style.display = "none";
+      }
+    });
+  }
+});
 
 
 //Función que elimina el producto del carrito:
@@ -337,9 +342,7 @@ function eliminarDelCarrito(id) {
 
 const vaciarCarrito = document.getElementById("vaciarCarrito");
 vaciarCarrito.addEventListener("click", () => {
-  
   eliminarTodoElCarrito();
-  
 });
 
 const eliminarTodoElCarrito = () => {
@@ -348,11 +351,7 @@ const eliminarTodoElCarrito = () => {
   // eliminamos la tabla de la página
   const tabla = contenedorCarrito.querySelector("table");
   contenedorCarrito.removeChild(tabla);
-  
-  //localStorage:
+
+  //limpiamos el localStorage:
   localStorage.clear();
-  
 };
-
-
-
